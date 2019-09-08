79db882d04bc63028eb02464d2ff8a13785f1852
Ruslan <russ-i@yandex.ru>
2019-09-08 19:22:41 +0300

    Initial commit

---
+++ gulp/config/tasks.js
@@ -0,0 +1,8 @@
+module.exports = [
+	'./gulp/tasks/pug',
+	'./gulp/tasks/sass',
+	'./gulp/tasks/watch',
+	'./gulp/tasks/server',
+	'./gulp/tasks/scripts',
+	'./gulp/tasks/image'
+];
---
+++ src/static/js/main.js
@@ -0,0 +1,16 @@
+ymaps.ready( init );
+
+function init() {
+	var myMap       = new ymaps.Map( "map", {
+		    center : [ 55.755549, 37.611850 ],
+		    zoom : 13,
+		    controls : []
+	    } ),
+	    myPlacemark = new ymaps.Placemark( [ 55.755549, 37.611850 ], {
+		    // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+		    balloonContentBody : "Встречаемся тут!",
+	    }, {
+		    preset: 'islands#redDotIcon'
+	    } );
+	myMap.geoObjects.add( myPlacemark );
+}
